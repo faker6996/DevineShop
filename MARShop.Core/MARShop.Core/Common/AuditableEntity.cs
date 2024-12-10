538a3f2bfcc81d55914028ee216030e8e86310bd
bachtran69 <bachtran69.traihonguyen2001@gmail.com>
2024-12-10 17:50:17 +0700

    pending

--- MARShop.Core/MARShop.Core/Common/AuditableEntity.cs
+++ MARShop.Core/MARShop.Core/Common/AuditableEntity.cs
@@ -4,7 +4,7 @@ namespace MARShop.Core.Common
 {
     public class AuditableEntity
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public DateTime Created { get; set; }
         public DateTime? LastModified { get; set; }
         public bool IsDelete { get; set; }
